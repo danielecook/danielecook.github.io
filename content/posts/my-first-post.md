205c56066ecc068590bd103fc6270fc930d44607
Daniel Cook <danielecook@gmail.com>
2019-06-24 11:30:55 +0100

    rebuilding site Mon Jun 24 11:30:55 BST 2019

--- content/posts/my-first-post.md
+++
@@ -1,6 +0,0 @@
----
-title: "My First Post"
-date: 2019-06-22T11:37:14+01:00
-draft: true
----
-
--- docs/photos/jhead_script.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-for i in `ls IMG*`; do
-	onvert IMG_0789.JPG -resize 2000 IMG_0789.JPG
-done;
-./jhead -autorot *
-./jhead -n%Y-%m-%d *
-./jhead -ft *
